f6db1c3ef06fb49867a14832c6af33701c9db4ce
Polina7777 <polina.produho98@mail.ru>
2023-08-30 20:15:24 +0200

    fix:fix grid

--- components/TheGrid.vue
+++ components/TheGrid.vue
@@ -1,38 +1,78 @@
-<script setup>
-const gridArr = new Array(11)
+<script>
+import { productsApi } from "../api-requests/products-api";
+
+
+export default{
+  mounted(){
+this.getItems()
+  },
+  data(){
+    return{
+     gridArr:[],
+    }
+  },
+  methods:{
+    async getItems(values) {
+      const products = await productsApi.getAllProducts();
+      products.push(...products);
+      products.push(...products);
+   this.gridArr = products
+}
+  }
+}
+// async function () {
+//       const products = await productsApi.getAllProductsFromBasket();
+//       const arr = products.map((item) => {
+//         return { product: item, count: item.attributes.basket_count };
+//       });
+//       this.productsFromBasketList = arr;
+   
+//     }
+
+
+
 </script>
 <template>
-    <div class="archive">
-<article  v-for="(item,index) in gridArr" :key="index" class="article">
-    <li></li>
-</article>
+    <div  class="archive">
+<div  v-for="(item,index) in gridArr" :key="index" class="article">
+    <TheCard :card="item"/>
+    </div>
 </div>
 </template>
 <style>
 .archive {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(210px, 1fr));
-  grid-gap: 32px;
+  grid-gap: 21px;
   grid-auto-flow: dense;
 }
 .article{
-    background: rgb(158, 231, 231);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #b49696;
+border-radius: 10px;
+background: #f9f5f5;
 }
 /* Extra-wide grid-posts */
 .article:nth-child(31n + 1) {
+
+background: #caa9a9;
   grid-column: 1 / -1;
 }
-.article:nth-child(16n + 2) {
-    background: rgba(128, 0, 64, 0.8);
+/* .article:nth-child(16n + 2) {
+border: 1px solid #b49696;
+border-radius: 10px;
+background: #caa9a9;
   grid-column: -3 / -1;
-}
+} */
 .article:nth-child(16n + 10) {
   grid-column: 1 / -2;
-  /* background: rgba(128, 0, 64, 0.8); */
+background: #dee7cf;
 }
-.article:nth-child(8n + 2) {
-  /* background: rgba(128, 0, 64, 0.8); */
-  /* Other shared syling */
+.article:nth-child(7n + 2) {
+  background: #cfe7df;
 }
 
 /* Single column display for phones */
